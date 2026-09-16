9e81963554120bec607703a4c0c0ddf430fecb4d
hitman <slomesh62@gmail.com>
2026-09-16 16:50:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Replace with your API key or configure it through the in-app ⚙️ Settings dialog.
 // Keys can be restricted in Google Cloud Console by website domain and to the YouTube Data API v3.
 window.CONFIG = {
-  YOUTUBE_API_KEY: "AIzaSyCJsqRWPpDJc3P8vUEYgC55Gn7vpaqXcCw"
+  YOUTUBE_API_KEY: ""
 };
